ca287147162aa78c1bfca9a973577d319aaee95a
wuxiao <1291383707@qq.com>
2019-11-30 22:50:06 +0800

    tic-tac-toe-react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cells = [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null],
+]
+const Cell = function (props){
+    return(
+        <div className="cell">
+            {props.text}
+        </div>
+    )
+}
+const [n, setN] = useState(0)
+const tell = () => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+const onClickCell = (row,col) => {
+    // n + 1
+    setN(n + 1)
+    // 改变 cells
+    const copy = JSON.parse(JSON.stringify(cells))
+    copy[row][col] = n % 2 === 0 ? 'X' : 'O'
+    setCells(copy)
+    // 判断输赢
+    tell()
+}
+const Chessboard = function(){
+    return (
+        <div>
+            <div>n:{n}</div>
+            {cells.map((items,row) =><div className="row">
+                {items.map((item, col) =><div className="col">
+                    <Cell text={item} onClick={() => onClickCell(row,col)}/>
+                </div>)}
+             </div>)}
+        </div>
+    )
+}
+
+ReactDOM.render(<Chessboard />, document.getElementById('root'));
